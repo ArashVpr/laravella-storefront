169a7986f54d7449532e5f83205e81435b91f4bb
Arash <avafapour@innovqube.com>
2025-05-02 14:25:49 +0200

    login functionality test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,7 +16,9 @@ class AuthTest extends TestCase
     {
         $response = $this->get('/login');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+        ->assertSee('Login')
+        ->assertSee('<a href="'. route('password.forgot').'"', false);
     }
     public function test_success_on_signup_page(): void
     {
@@ -28,6 +31,35 @@ class AuthTest extends TestCase
         $response = $this->get('/forgot-password');
 
         $response->assertStatus(200);
+    }
+    public function test_incorrect_credentials_on_login_page(): void
+    {
+        User::factory()->create([
+            'email' => 'lexi@yahoo.com',
+            'password' => bcrypt('password'),
+        ]);
+        $response = $this->post(route('login.store'), [
+            'email' => 'lexi@yahoo.com',
+            'password' => 'wrong-password',
+        ]);
+
+        $response->assertFound()
+        // ->assertSessionHasErrors(['email'])
+        ->assertInvalid(['email']);
+    }
+    public function test_correct_credentials_on_login_page(): void
+    {
+        User::factory()->create([
+            'email' => 'lexi@yahoo.com',
+            'password' => bcrypt('password'),
+        ]);
+        $response = $this->post(route('login.store'), [
+            'email' => 'lexi@yahoo.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertFound()
+        ->assertSessionHas(['success']);
     }
 
 
