ee757c507dd2174015e138f684b119a0f89e045c
Arash <avafapour@innovqube.com>
2025-05-02 15:35:24 +0200

    signup tests

---
+++ tests/Feature/SignupTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SignupTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_signup_functionality(): void
+    {
+        $response = $this->get('/signup');
+        $response = $this->post(route('signup.store'), [
+            'name' => 'Lexi',
+            'email' => 'lexi@yahoo.com',
+            'phone' => '12345678',
+            'password' => 'passworD@%^*1234',
+            'password_confirmation' => 'passworD@%^*1234',
+        ]);
+
+        $response->assertFound()
+        ->assertRedirectToRoute('homepage')
+        ->assertSessionHas(['success']);
+    }
+    public function test_errors_on_signup_page(): void
+    {
+        $response = $this->get('/signup');
+        $response = $this->post(route('signup.store'), [
+            'name' => '',
+            'email' => 'lexiyahoo.com',
+            'phone' => '1234567',
+            'password' => 'passworD@%^*12345',
+            'password_confirmation' => 'passworD@%^*1234',
+        ]);
+
+        $response->assertFound()
+        ->assertInvalid(['name', 'email', 'phone', 'password']);
+
+    }
+    public function test_email_and_phone_errors_on_signup_page(): void
+    {
+        $response = $this->get('/signup');
+        $response = $this->post(route('signup.store'), [
+            'name' => 'Lexi',
+            'email' => 'lexiyahoo.com',
+            'phone' => '1234567',
+            'password' => 'passworD@%^*1234',
+            'password_confirmation' => 'passworD@%^*1234',
+        ]);
+
+        $response->assertFound()
+        ->assertInvalid(['email', 'phone']);
+
+    }
+    public function test_empty_field_errors_on_signup_page(): void
+    {
+        $response = $this->get('/signup');
+        $response = $this->post(route('signup.store'), [
+            'name' => '',
+            'email' => '',
+            'phone' => '',
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+
+        // $response->ddSession();
+
+        $response->assertFound()
+        ->assertInvalid(['name', 'email', 'phone', 'password']);
+
+    }
+}
