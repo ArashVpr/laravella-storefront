67eebcab2f3c1fd3b1d4fc5603b2e1baf4538b30
Arash <avafapour@innovqube.com>
2025-08-23 13:27:08 +0200

    debug: deployment

--- resources/views/components/announcement-bar.blade.php
+++ resources/views/components/announcement-bar.blade.php
@@ -1,3 +1,3 @@
 <div class="announcement-bar" role="banner" aria-label="Site announcement">
-    <p>This is a demo application with limited functionalities.</p>
+    <p>This is a demo application with limited functionalities TESTTTTTTT!!!!.</p>
 </div>
